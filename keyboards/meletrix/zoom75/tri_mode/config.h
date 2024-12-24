2a95b3e8c49d3d07a818f0b04936b78e71f60217
Mark Hooijberg <m.hooijberg24@gmail.com>
2024-12-24 18:44:02 +0100

    Moved files from personal repository, to shared repository.

---
+++ keyboards/meletrix/zoom75/tri_mode/config.h
@@ -0,0 +1,7 @@
+// Copyright 2024 MHooijberg <https://github.com/MHooijberg>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef AT32_HEXTCLK
+#define AT32_HEXTCLK 16000000
---
+++ keyboards/meletrix/zoom75/tri_mode/mcuconf.h
@@ -0,0 +1,35 @@
+// Copyright 2024 MHooijberg <https://github.com/MHooijberg>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include_next <mcuconf.h>
+
+// 16MHz HEXTCLK
+#undef AT32_HEXT_ENABLED
+#undef AT32_PLLRCS
+#undef AT32_PLLHEXTDIV
+#undef AT32_PLLMULT_VALUE
+#undef AT32_PLL_FR_VALUE
+#undef AT32_PLL_MS_VALUE
+#undef AT32_PLL_NS_VALUE
+#undef AT32_AHBDIV
+#undef AT32_APB1DIV
+#undef AT32_APB2DIV
+#undef AT32_ADCDIV
+#undef AT32_USBDIV
+#undef AT32_PVM_ENABLE
+
+#define AT32_HEXT_ENABLED                   TRUE
+#define AT32_PLLRCS                         AT32_PLLRCS_HEXT
+#define AT32_PLLHEXTDIV                     AT32_PLLHEXTDIV_DIV2
+#define AT32_PLLMULT_VALUE                  18
+#define AT32_PLL_FR_VALUE                   4
+#define AT32_PLL_MS_VALUE                   1
+#define AT32_PLL_NS_VALUE                   72
+#define AT32_AHBDIV                         AT32_AHBDIV_DIV1
+#define AT32_APB1DIV                        AT32_APB1DIV_DIV2
+#define AT32_APB2DIV                        AT32_APB2DIV_DIV2
+#define AT32_ADCDIV                         AT32_ADCDIV_DIV4
+#define AT32_USBDIV                         AT32_USBDIV_DIV3
+#define AT32_PVM_ENABLE                     FALSE
