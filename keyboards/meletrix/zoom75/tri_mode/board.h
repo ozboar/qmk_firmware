ba1209fffbd603d1714049ed88f11151a043dac1
Mark Hooijberg <m.hooijberg24@gmail.com>
2025-01-09 20:36:24 +0100

    Working on EEPROM and clean up

--- keyboards/meletrix/zoom75/tri_mode/board.h
+++ keyboards/meletrix/zoom75/tri_mode/board.h
@@ -1,5 +1,9 @@
 #pragma once
 #include_next <board.h>
 
+// Undefine the KB family variant.
 #undef AT32F415KB
+
+// Add a definition to describe this,
+// project uses the RC family variant.
 #define AT32F415RC
--- keyboards/meletrix/zoom75/tri_mode/config.h
+++ keyboards/meletrix/zoom75/tri_mode/config.h
@@ -4,3 +4,14 @@
 
 #undef AT32_HEXTCLK
 #define AT32_HEXTCLK 16000000
+
+// Define EEPROM config.
+#define EXTERNAL_EEPROM_SPI
+#define EXTERNAL_EEPROM_SIZE 0x80000  // 4M-bit (512KB)
+#define EXTERNAL_EEPROM_PAGE_SIZE 256
+
+// Define SPI config for AT32F415 Implementation.
+#define SPI_SCLK_PIN B13
+#define SPI_MOSI_PIN B15
+#define SPI_MISO_PIN B14
+#define SPI_CS_PIN   B12
