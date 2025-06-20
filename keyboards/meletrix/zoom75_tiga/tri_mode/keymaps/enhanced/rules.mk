c614e12f0c7ee8e1a4c082082a6d0122dbf053a2
Mark <m.hooijberg24@gmail.com>
2025-06-20 13:08:09 +0200

    Seperating out settings.

--- keyboards/meletrix/zoom75_tiga/tri_mode/keymaps/enhanced/rules.mk
+++ keyboards/meletrix/zoom75_tiga/tri_mode/keymaps/enhanced/rules.mk
@@ -3,3 +3,79 @@ ENCODER_MAP_ENABLE = yes
 KEYBOARD_SHARED_EP = yes
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
+
+    # "bootmagic": {
+    #     "enabled": true,
+    #     "matrix": [ 0, 0 ]
+    # },
+    # "caps_word": {
+    #     "both_shifts_turns_on": false,
+    #     "double_tap_shift_turns_on": false,
+    #     "enabled": false,
+    #     "idle_timeout": 5000,
+    #     "invert_on_shift": false
+    # },
+    # "combo": {
+    #     "term": 50
+    # },
+    # "features": {
+    #     "bootmagic": true,
+    #     "command": true,
+    #     "console": true,
+    #     "encoder": true,
+    #     "extrakey": true,
+    #     "mousekey": true,
+    #     "nkro": true
+    # },
+    # "host": {
+    #     "default": {
+    #         "nkro": false
+    #     }
+    # },
+	# "leader_key": {
+    #     "timing": false,
+    #     "strict_processing": false,
+    #     "timeout": 300
+    # },
+    # "mouse_key": {
+    #     "delay": 150,
+    #     "enabled": false,
+    #     "interval": 16,
+    #     "max_speed": 32,
+    #     "time_to_max": 32,
+    #     "wheel_delay": 10
+    # },
+    # "oneshot": {
+    #     "tap_toggle": 5,
+    #     "timeout": 5000
+    # },
+    # "qmk": {
+    #     "locking": {
+    #         "enabled": false,
+    #         "resync": false
+    #     },
+    #     "tap_capslock_delay": 80,
+    #     "tap_keycode_delay": 0
+    # },
+    # "secure": {
+    #     "enabled": false,
+    #     "idle_timeout": 60000, // 60 seconds.
+    #     "unlock_sequence": [[0, 0], [0, 1], [4, 3]], // Escape, F1, X.
+    #     "unlock_timeout": 5000 // 5 seconds.
+    # },
+    # "stenography": {
+    #     "enabled": false,
+    #     "protocol": "all"
+    # },
+    # "tapping": {
+    #     "chordal_hold": false,
+    #     "hold_on_other_key_press": false,
+    #     "hold_on_other_key_press_per_key": false,
+    #     "permissive_hold": false,
+    #     "permissive_hold_per_key": false,
+    #     "retro": false,
+    #     "retro_per_key": false,
+    #     "term": 200,
+    #     "term_per_key": false,
+    #     "toggle": 5
+    # },
